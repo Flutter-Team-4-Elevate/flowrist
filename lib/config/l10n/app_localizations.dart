58f32ad574b0ebb1b72859fc3d70be32529d44e0
Mahmoud Abu-Hijazy <mahmoud.ibrahim.saad.750@gmail.com>
2026-09-12 16:17:08 +0300

    feat(auth): localize forgot password app bar title
    - add `forgetPasswordText` localization string in `app_en.arb` and regenerated localization classes.
    - update `ForgetPasswordView` app bar title to use `AppLocalizations.of(context)!.forgetPasswordText` instead of a hardcoded string.

--- lib/config/l10n/app_localizations.dart
+++ lib/config/l10n/app_localizations.dart
@@ -364,6 +364,12 @@ abstract class AppLocalizations {
   /// **'Forgot Password?'**
   String get forgotPassword;
 
+  /// No description provided for @forgetPasswordText.
+  ///
+  /// In en, this message translates to:
+  /// **'Forgot Password'**
+  String get forgetPasswordText;
+
   /// No description provided for @forgotPasswordDescription.
   ///
   /// In en, this message translates to:
